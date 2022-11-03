e47501a421ff7a1cee3eb82f6d92aed10dd306f2
Explicit12 <explicit1@tutanota.com>
2022-11-04 00:03:54 +0200

    created VideoCompact component

---
+++ src/components/videoCompact/VideoCompact.vue
@@ -0,0 +1,91 @@
+<script setup lang="ts">
+  import { computed } from "vue";
+  import { useI18n } from "vue-i18n";
+  import { RouterLink } from "vue-router";
+  import { IconPlay } from "@iconify-prerendered/vue-mdi";
+  import dayjs from "dayjs";
+  import relativeTime from "dayjs/plugin/relativeTime";
+
+  dayjs.extend(relativeTime);
+
+  const props = defineProps<{
+    name: string;
+    author: { name: string; link: string };
+    date: number;
+    views: number;
+    image: string | null;
+  }>();
+
+  const { t } = useI18n();
+
+  const formatedViews = computed<string>(() => {
+    const views: number = props.views;
+
+    if (Math.abs(views) <= 999) {
+      return String(Math.sign(views) * Math.abs(views));
+    } else if (Math.abs(views) > 999 && Math.abs(views) < 1_000_000) {
+      return String(Number((Math.abs(views) / 1000).toFixed(1))) + "k";
+    } else {
+      return String(Number((Math.abs(views) / 1_000_000).toFixed(1))) + "M";
+    }
+  });
+
+  const formatedDate = computed<string>(() => {
+    return dayjs(props.date).fromNow();
+  });
+</script>
+
+<template>
+  <div>
+    <RouterLink :to="{ name: 'home' }">
+      <img
+        v-if="image"
+        :src="image"
+        alt="name"
+        class="aspect-video rounded-lg object-cover"
+      />
+      <div
+        v-else
+        class="flex aspect-video items-center justify-center rounded-lg bg-gray-200"
+      >
+        <IconPlay width="32" height="32" class="text-gray-400" />
+      </div>
+    </RouterLink>
+
+    <RouterLink
+      :to="{ name: 'home' }"
+      class="block pt-4 font-sans text-base font-normal text-gray-900 line-clamp-2"
+    >
+      {{ name }}
+    </RouterLink>
+
+    <div>
+      <RouterLink
+        :to="{ name: 'home' }"
+        class="pt-2 font-sans text-sm font-normal text-gray-400 line-clamp-1 hover:underline"
+      >
+        {{ author.name }}
+      </RouterLink>
+      <span class="font-sans text-sm font-normal text-gray-400">
+        {{ formatedViews + " " + t("views") }}
+      </span>
+      <span class="pl-2 font-sans text-sm font-normal text-gray-400">
+        {{ formatedDate }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<i18n lang="json">
+{
+  "en-US": {
+    "views": "views"
+  },
+  "uk-UA": {
+    "views": "переглядів"
+  },
+  "ru-RU": {
+    "views": "просмотров"
+  }
+}
+</i18n>
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/style.css";
+import "dayjs/locale/en";
+import "dayjs/locale/uk";
+import "dayjs/locale/ru";
 
 const app = createApp(App);
 
