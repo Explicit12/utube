8a9ab7df13df4d8dd78041ca30ca43f00d1bf153
Explicit12 <explicit1723@gmail.com>
2022-11-09 16:27:20 +0200

    moved to components

--- src/components/VideoCompact.vue
+++ src/components/VideoCompact.vue
@@ -7,35 +7,32 @@
   import relativeTime from "dayjs/plugin/relativeTime";
 
   import { pingImage } from "@/utils/invidiousAPI";
+  import { formatNumbers } from "@/helpers/formatNumbers";
 
   import type { Ref } from "vue";
-  import type { VideoThumbnails } from "@/utils/invidiousAPI";
+  import type {
+    VideoThumbnails,
+    ChannelsId,
+    VideoId,
+  } from "@/utils/invidiousAPI";
 
   dayjs.extend(relativeTime);
 
   const props = defineProps<{
     name: string;
-    author: { name: string; id: string };
+    author: { name: string; id: ChannelsId };
+    videoId: VideoId;
     date: number;
     views: number;
     image: VideoThumbnails | null;
+    horizontalLayout?: boolean;
   }>();
 
   const imageError: Ref<boolean> = ref(false);
 
   const { t } = useI18n();
 
-  const formatedViews = computed<string>(() => {
-    const views: number = props.views;
-
-    if (Math.abs(views) <= 999) {
-      return String(Math.sign(views) * Math.abs(views));
-    } else if (Math.abs(views) > 999 && Math.abs(views) < 1_000_000) {
-      return String(Number((Math.abs(views) / 1000).toFixed(1))) + "k";
-    } else {
-      return String(Number((Math.abs(views) / 1_000_000).toFixed(1))) + "M";
-    }
-  });
+  const formatedViews = computed<string>(() => formatNumbers(props.views));
 
   const formatedDate = computed<string>(() => {
     return dayjs.unix(props.date).fromNow();
@@ -50,10 +47,15 @@
 </script>
 
 <template>
-  <div>
-    <RouterLink :to="{ name: 'home' }">
+  <RouterLink
+    :to="{ name: 'home' }"
+    :class="{ 'md:flex md:gap-4': horizontalLayout }"
+    class="block"
+  >
+    <RouterLink :to="{ name: 'home' }" @click.stop>
       <img
         v-if="image && !imageError"
+        :class="{ 'md:min-w-[24rem] md:max-w-sm': horizontalLayout }"
         :src="image[3].url"
         :alt="name"
         decoding="async"
@@ -65,34 +67,39 @@
       />
       <div
         v-else
+        :class="{ 'md:min-w-[24rem] md:max-w-sm': horizontalLayout }"
         class="flex aspect-video items-center justify-center rounded-lg bg-gray-200"
       >
         <IconPlay width="32" height="32" class="text-gray-400" />
       </div>
     </RouterLink>
 
-    <RouterLink
-      :to="{ name: 'home' }"
-      class="block pt-4 font-sans text-base font-normal text-gray-900 line-clamp-2"
-    >
-      {{ name }}
-    </RouterLink>
-
     <div>
       <RouterLink
         :to="{ name: 'home' }"
-        class="pt-2 font-sans text-sm font-normal text-gray-400 line-clamp-1 hover:underline"
+        :class="{ 'pt-4': !horizontalLayout, 'max-md:pt-4': horizontalLayout }"
+        class="block font-sans text-base font-normal text-gray-900 line-clamp-2"
       >
-        {{ author.name }}
+        {{ name }}
       </RouterLink>
-      <span class="font-sans text-sm font-normal text-gray-400">
-        {{ formatedViews + " " + t("views") }}
-      </span>
-      <span class="pl-2 font-sans text-sm font-normal text-gray-400">
-        {{ formatedDate }}
-      </span>
+
+      <div>
+        <RouterLink
+          :to="{ name: 'home' }"
+          class="pt-2 font-sans text-sm font-normal text-gray-400 line-clamp-1 hover:underline"
+          @click.stop
+        >
+          {{ author.name }}
+        </RouterLink>
+        <span class="font-sans text-sm font-normal text-gray-400">
+          {{ formatedViews + " " + t("views") }}
+        </span>
+        <span class="pl-2 font-sans text-sm font-normal text-gray-400">
+          {{ formatedDate }}
+        </span>
+      </div>
     </div>
-  </div>
+  </RouterLink>
 </template>
 
 <i18n lang="json">
