ebddbfd1250e02ae6e3b4d35bb7c0041b6495f8c
Explicit12 <explicit1723@gmail.com>
2022-11-09 16:38:50 +0200

    created VideoCompact component

---
+++ src/components/ChannelCompact.vue
@@ -0,0 +1,105 @@
+<script setup lang="ts">
+  import { onBeforeMount, ref, computed } from "vue";
+  import { useI18n } from "vue-i18n";
+  import { RouterLink } from "vue-router";
+  import { storeToRefs } from "pinia";
+
+  import PrimaryButton from "@/components/buttons/PrimaryButton.vue";
+  import SecondaryButton from "@/components/buttons/SecondaryButton.vue";
+
+  import { pingImage } from "@/utils/invidiousAPI";
+  import { useUserData } from "@/stores/userData";
+  import { formatNumbers } from "@/helpers/formatNumbers";
+
+  import type { Ref } from "vue";
+  import type { AuthorThumbnails, ChannelsId } from "@/utils/invidiousAPI";
+
+  const props = defineProps<{
+    name: string;
+    subs: number;
+    channelsId: ChannelsId;
+    thumbnail: AuthorThumbnails;
+  }>();
+
+  const { t } = useI18n();
+  const userData = useUserData();
+
+  const { subscribeToChannel, unsubscribeFromChannel } = userData;
+  const { subscriptions } = storeToRefs(userData);
+
+  const imageError: Ref<boolean> = ref(false);
+
+  const formatedSubs = computed<string>(() => formatNumbers(props.subs));
+
+  onBeforeMount(() => {
+    if (props.thumbnail) {
+      // Ping image url to check whether it exists or not
+      pingImage(props.thumbnail[0].url).catch(() => (imageError.value = true));
+    }
+  });
+</script>
+
+<template>
+  <RouterLink :to="{ name: 'home' }" class="flex justify-between gap-4">
+    <div class="aspect-square max-h-[56px] min-w-[56px] max-w-[56px]">
+      <img
+        v-if="thumbnail && !imageError"
+        :src="thumbnail[3].url"
+        decoding="async"
+        referrerpolicy="no-referrer"
+        crossorigin="anonymous"
+        loading="lazy"
+        :alt="t('alt-avatar')"
+        class="h-full w-full rounded-lg"
+        width="32"
+        height="32"
+      />
+      <div v-else class="h-full w-full rounded-lg bg-gray-200" />
+    </div>
+    <div class="w-full overflow-hidden">
+      <p class="font-sans text-base font-semibold text-gray-900 line-clamp-1">
+        {{ name }}
+      </p>
+      <p class="min-w-max pt-2 font-sans text-sm text-gray-400">
+        {{ formatedSubs + " " + t("subscriptions") }}
+      </p>
+    </div>
+    <PrimaryButton
+      v-if="!subscriptions.includes(channelsId)"
+      class="self-start"
+      @click.stop.prevent="subscribeToChannel(channelsId)"
+    >
+      {{ t("subscribe") }}
+    </PrimaryButton>
+    <SecondaryButton
+      v-else
+      class="self-start"
+      @click.stop.prevent="unsubscribeFromChannel(channelsId)"
+    >
+      {{ t("unsubscribe") }}
+    </SecondaryButton>
+  </RouterLink>
+</template>
+
+<i18n lang="json">
+{
+  "en-US": {
+    "alt-avatar": "Channel avatar",
+    "subscriptions": "Subs",
+    "subscribe": "Subscribe",
+    "unsubscribe": "Unsubscribe"
+  },
+  "uk-UA": {
+    "alt-avatar": "Аватар каналу",
+    "subscriptions": "Підписчиків",
+    "subscribe": "Підписатися",
+    "unsubscribe": "Відписатися"
+  },
+  "ru-RU": {
+    "alt-avatar": "Аватар канала",
+    "subscriptions": "Подписчиков",
+    "subscribe": "Подписаться",
+    "unsubscribe": "Отписаться"
+  }
+}
+</i18n>
---
+++ src/components/skeletonLoaders/ChannelCompactSkeleton.vue
@@ -0,0 +1,13 @@
+<template>
+  <span class="flex animate-pulse justify-between gap-4">
+    <div
+      class="aspect-square h-full w-full min-w-[56px] max-w-[56px] rounded-lg bg-gray-200"
+    />
+
+    <div class="basis-full">
+      <div class="mt-2 h-4 w-3/12 rounded-sm bg-gray-400" />
+      <div class="mt-2 h-2 w-2/12 rounded-sm bg-gray-200" />
+    </div>
+    <div class="h-11 w-32 self-start rounded-lg bg-gray-200" />
+  </span>
+</template>
