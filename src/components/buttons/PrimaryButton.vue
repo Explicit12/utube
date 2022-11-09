b0424f20cf40c8f4668a71ba3e08bcc66278649c
Explicit12 <explicit1723@gmail.com>
2022-11-09 16:40:27 +0200

    created PrimaryButton component

---
+++ src/components/buttons/PrimaryButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    class="rounded-lg border-2 border-transparent bg-blue-600 py-2 px-4 text-center font-sans text-base font-normal text-white transition-colors hover:bg-blue-700 focus:border-2 focus:border-blue-700"
+  >
+    <slot />
+  </button>
+</template>
