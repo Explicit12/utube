57e944a32dcf81a17a48f3afe13aac9146418f72
Explicit12 <explicit1723@gmail.com>
2022-11-15 20:26:22 +0200

    removed unnecessary styles

--- src/components/TheError.vue
+++ src/components/TheError.vue
@@ -11,13 +11,13 @@
   <div
     class="mt-4 flex w-full flex-col justify-center self-center rounded-lg border-8 border-dashed border-orange-400 p-4 text-center"
   >
-    <div class="flex items-center gap-2">
+    <div class="flex gap-2">
       <IconAlertCircleOutline class="text-gray-900" width="32" height="32" />
       <h1 class="font-sans text-xl font-bold text-gray-900">
         {{ t("error") }}
       </h1>
     </div>
-    <p class="pt-2 text-left font-sans text-base font-normal text-gray-900">
+    <p class="pt-2 font-sans text-base font-normal text-gray-900">
       {{ message }}
     </p>
   </div>
