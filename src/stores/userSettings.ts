e31696bd6c9174431170812f94a2a056034d77bb
Explicit12 <explicit1723@gmail.com>
2022-11-28 08:27:06 +0200

    removed type annotations where typescript can infer it by itself

--- src/stores/userSettings.ts
+++ src/stores/userSettings.ts
@@ -1,11 +1,9 @@
 import { defineStore } from "pinia";
 import { useLocalStorage } from "@vueuse/core";
 
-import type { Ref } from "vue";
-
 export const useUserSettings = defineStore("userSettings", () => {
-  const isMenuOpen: Ref<boolean> = useLocalStorage("isMenuOpen", true);
-  const userLocale: Ref<string> = useLocalStorage("userLocale", "en-US");
+  const isMenuOpen = useLocalStorage("isMenuOpen", true);
+  const userLocale = useLocalStorage("userLocale", "en-US");
 
   function toggleMenu(): void {
     isMenuOpen.value = !isMenuOpen.value;
