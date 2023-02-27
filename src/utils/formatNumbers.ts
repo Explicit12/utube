749b2a7e807f753fed0c49bbcfeb11f69ffc1243
Explicit12 <explicit1723@gmail.com>
2023-02-27 23:06:17 +0200

    refactored code. Fixed bug when function returns rounded value

--- src/utils/formatNumbers.ts
+++ src/utils/formatNumbers.ts
@@ -1,11 +1,16 @@
 export default function formatNumbers(
   number: number,
 ): string | `${number}k` | `${number}M` {
-  if (Math.abs(number) <= 999) {
-    return String(Math.sign(number) * Math.abs(number));
-  } else if (Math.abs(number) > 999 && Math.abs(number) < 1_000_000) {
-    return String(Number((Math.abs(number) / 1000).toFixed(1))) + "k";
+  const sign = Math.sign(number);
+  const abs = Math.abs(number);
+
+  if (abs <= 999) {
+    return String(sign * abs);
+  } else if (abs > 999 && abs < 1_000_000) {
+    // We have to use 1000.1 instead of 1000 to avoind rounding
+    return String(sign * Number((abs / 1000.1).toFixed(1))) + "k";
   } else {
-    return String(Number((Math.abs(number) / 1_000_000).toFixed(1))) + "M";
+    // Same here
+    return String(sign * Number((abs / 1_000_060).toFixed(1))) + "M";
   }
 }
